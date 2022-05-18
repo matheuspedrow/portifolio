5255d29ff96cb6017c24ef912c1d042bf66127b2
Matheus <matheuspedrow@gmail.com>
2022-05-18 18:33:12 -0300

    att

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import { FaArrowRight } from 'react-icons/fa'
 import { Container, Content, Infos, Name, Function, Intro, LinkProjects, Logo, Img } from '../../styles/indexStyle'
 import Link from 'next/link'
 
-export default function Home() {
+function Home() {
   return (
    <Container>
      <Content>
@@ -25,3 +25,5 @@ export default function Home() {
    </Container>
   )
 }
+
+export default Home
